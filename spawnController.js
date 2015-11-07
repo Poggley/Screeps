49769f4e38c615540b779d7d04ba190629622e9a
micboyd <mboyd855@gmail.com>
2015-11-07 16:55:25 +0000

    Defender

--- spawnController.js
+++ spawnController.js
@@ -4,7 +4,7 @@ module.exports = {
   // Decides which creep to make and creates it
   makeCreep: function (spawn) {
     var creep_role = 'harvester'; // TODO get this from elsewhere
-    var creep_type = require(creep_type);
+    var creep_type = require(creep_role);
     var proto = new creep_type.creepProto();
 
     spawn.createCreep(proto.bodyparts, null, { role : creep_role });

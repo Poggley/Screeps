39134d6bde3951e9757aabd977ffaeb70d770f39
Poggley <poggley@gmail.com>
2015-11-07 17:37:21 +0000

    beautification :D

--- harvester.js
+++ harvester.js
@@ -4,16 +4,16 @@
 function creepProto(creep) {
   this.bodyparts = [WORK, CARRY, MOVE];
 
-  this.action = function() {
-    	if(creep.carry.energy < creep.carryCapacity) {
-    		var sources = creep.room.find(FIND_SOURCES);
-    		creep.moveTo(sources[0]);
-    		creep.harvest(sources[0]);
-    	}
-    	else {
-    		creep.moveTo(Game.spawns.Spawn1);
-    		creep.transferEnergy(Game.spawns.Spawn1)
-    	}
+  this.action = function () {
+    if (creep.carry.energy < creep.carryCapacity) {
+      var sources = creep.room.find(FIND_SOURCES);
+      creep.moveTo(sources[0]);
+      creep.harvest(sources[0]);
+    }
+    else {
+      creep.moveTo(Game.spawns.Spawn1);
+      creep.transferEnergy(Game.spawns.Spawn1)
+    }
   }
 };
 
--- spawnController.js
+++ spawnController.js
@@ -7,27 +7,27 @@ module.exports = {
     var creep_type = require(creep_role);
     var proto = new creep_type.creepProto();
 
-    spawn.createCreep(proto.bodyparts, null, { role : creep_role });
+    spawn.createCreep(proto.bodyparts, null, { role: creep_role });
   },
 
   // Sets up flags for that spawn
   flagCreate: function (spawn) {
-      if(!spawn.room.lookForAt('flag',25,25)[0]) {
-          spawn.room.createFlag(25,25);
-      }
-      var gatherFlag;
-      if(spawn.room.lookForAt('flag',25,25)[0] && !gatherFlag) {
-          gatherFlag = spawn.room.lookForAt('flag',25,25)[0];
-          spawn.memory.gatherFlag = gatherFlag;
-      }
+    if (!spawn.room.lookForAt('flag', 25, 25)[0]) {
+      spawn.room.createFlag(25, 25);
+    }
+    var gatherFlag;
+    if (spawn.room.lookForAt('flag', 25, 25)[0] && !gatherFlag) {
+      gatherFlag = spawn.room.lookForAt('flag', 25, 25)[0];
+      spawn.memory.gatherFlag = gatherFlag;
+    }
 
-      if(!spawn.room.lookForAt('flag',21,25)[0]) {
-          spawn.room.createFlag(21,25);
-      }
-      var healFlag;
-      if(spawn.room.lookForAt('flag',21,25)[0] && !healFlag) {
-          healFlag = spawn.room.lookForAt('flag',21,25)[0];
-          spawn.memory.healFlag = healFlag;
-      }
+    if (!spawn.room.lookForAt('flag', 21, 25)[0]) {
+      spawn.room.createFlag(21, 25);
+    }
+    var healFlag;
+    if (spawn.room.lookForAt('flag', 21, 25)[0] && !healFlag) {
+      healFlag = spawn.room.lookForAt('flag', 21, 25)[0];
+      spawn.memory.healFlag = healFlag;
+    }
   }
 };

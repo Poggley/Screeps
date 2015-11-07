6efdc232ead0dca40ca260620c040193d300ae2c
Poggley <poggley@gmail.com>
2015-11-07 13:47:03 +0000

    Updated: spawnController, main.js

--- harvester.js
+++ harvester.js
@@ -6,7 +6,10 @@ var common;
 common = require('common');
 
 // Harvester.
-function Harvester() {};
+function Harvester() {
+  this.body = [WORK, CARRY, MOVE];
+};
+
 Harvester.inheritsFrom(Creep);        
-Harvester.prototype.body = [WORK, CARRY, MOVE];
+
 
